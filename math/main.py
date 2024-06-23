fcc3f9f4fc1b86f4d04ac1a1211b61a6dceae185
mvstermind <zablockidawid1@gmail.com>
2024-06-23 17:03:27 +0200

    working on giving user choice to pick one subject to work on

--- math/main.py
+++ math/main.py
@@ -1,28 +1,71 @@
 import flet as ft
-from mathgenerator import addition, multiplication, division, subtraction
+from mathgenerator import addition, division, multiplication, subtraction
+import time
 
-
-problem, solution = addition()
-problem, solution = problem.replace("$", ""), solution.replace("$", "")
+streak = 0
 
 
 def main(page):
+    user_choice = get_choice(page)
+    choice(page, user_choice)
+
+
+def get_choice(page):
+    def one_picked(e):
+        pass
+
+    page.add(ft.Text("Choose your subject"))
+    page.add(ft.ElevatedButton(text="division", on_click=one_picked))
+    page.add(ft.ElevatedButton(text="multiplication", on_click=one_picked))
+    page.add(ft.ElevatedButton(text="addition", on_click=one_picked))
+    page.add(ft.ElevatedButton(text="subtraction", on_click=one_picked))
+
+
+def gen_problem(page, problem_type):
+    global streak, problem, solution
+    problem, solution = problem_type()
+    problem, solution = problem.replace("$", ""), solution.replace("$", "")
+
     def btn_click(e):
+        global streak, problem, solution
         if not txt_name.value:
             txt_name.error_text = "This field cannot be empty"
-            page.update()
+            txt_name.update()
+            return False
         else:
             answer = txt_name.value
-            page.clean()
             if int(answer) == int(solution):
-                page.add(ft.Text("Good job homie"))
+                streak += 1
+                page.clean()
+                page.add(ft.Text(f"Good job homie, {streak} answers "))
+                time.sleep(2)
+                page.clean()
+                gen_problem(page, problem_type)
+                return True
             else:
-                page.add(ft.Text(f"Almost, answer is {answer}"))
+                page.clean()
+                page.add(ft.Text(f"Almost, answer is {solution}"))
+                time.sleep(2)
+                page.clean()
+                gen_problem(page, problem_type)
+                return True
 
     page.add(ft.Text(f"{problem}"))
     txt_name = ft.TextField(label="Answer")
 
     page.add(txt_name, ft.ElevatedButton("Submit", on_click=btn_click))
+
+
+def choice(page, choice):
+    match choice:
+        case "division":
+            gen_problem(page, division)
+        case "multiplication":
+            gen_problem(page, multiplication)
+        case "addition":
+            gen_problem(page, addition)
+        case "subtraction":
+            gen_problem(page, subtraction)
 
 
 ft.app(target=main)
